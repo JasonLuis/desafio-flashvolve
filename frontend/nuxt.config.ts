983fd733041390afb6881b64a8e1bb3934d675bb
JasonLuis <jason20luis@gmail.com>
2023-10-19 23:15:40 -0300

    chore: create web page in nuxt 3 and vue 3

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { quasar } from '@quasar/vite-plugin';
+
+export default defineNuxtConfig({
+  ssr: false,
+  srcDir: 'src/',
+  dir: {
+    pages: 'pages',
+    assets: 'core/assets',
+    layouts: 'core/layouts',
+    public: 'core/public'
+  },
+  components: {
+    dirs: ['core/components']
+  },
+  css: [
+    '@quasar/extras/roboto-font/roboto-font.css',
+    '@quasar/extras/material-icons/material-icons.css',
+    '~/core/assets/styles/quasar.sass'
+  ],
+  build: {
+    transpile: ['quasar']
+  },
+  plugins: ['~/core/plugins/quasar.client.ts'],
+  imports: {
+    dirs: ['~/core/composables/**']
+  },
+  vite: {
+    plugins: [
+      /* vue({
+              template: { transformAssetUrls }
+            }), */
+      quasar({
+        sassVariables: '~/core/assets/styles/quasar.variables.sass'
+      })
+    ]
+  }
+})
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="full-height">
+      <div class="full-height login">
+        <UiLoginCard @auth-operator="auth"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UiLoginCard from '../core/components/LoginCard/LoginCard.vue';
+
+function auth(login: { user: string, password: string}) {
+  alert(login.user + ' '+ login.password);
+}
+
+</script>
+
+<style scoped lang="scss">
+
+.container {
+  height: 100vh;
+  background-image: url(../core/assets/images/background.jpg);
+  background-size: cover; /* Faz a imagem cobrir toda a área da div */
+  background-position: center;
+  background-attachment: fixed;
+}
+
+.login {
+  width: 50%;
+  background: rgba(255, 255, 255, 0.90);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
