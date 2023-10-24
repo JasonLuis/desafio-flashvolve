de06938fdd29672a59e9e4fec434c89d8b78668f
JasonLuis <jason20luis@gmail.com>
2023-10-23 23:23:29 -0300

    fix: resolved bug

---
+++ frontend/src/core/layouts/message.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-drawer
+      v-model="leftDrawerOpen"
+      :width="380"
+      show-if-above
+      side="left"
+      bordered
+    >
+      <q-item>
+        <q-item-section side class="q-mt-lg q-ml-sm">
+          <q-avatar size="80px" rounded>
+            <img src="~~/core/assets/images/bot-image.png" alt="" />
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Mary</q-item-label>
+          <q-item-label caption class="text-black">Operador</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item class="row justify-center">
+        <q-input v-model="text" rounded outlined>
+          <template #append>
+            <q-avatar>
+              <q-icon size="30px" name="search" />
+            </q-avatar>
+          </template>
+        </q-input>
+      </q-item>
+      <q-separator class="q-mt-xl" />
+      <!-- drawer content -->
+
+      <q-list>
+        <q-item class="q-mt-lg">
+          <q-item-section side>
+            <q-avatar size="50px" rounded>
+              <img src="~~/core/assets/images/bot-image.png" alt="" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>
+              Mary
+              <q-badge color="teal">new</q-badge>
+            </q-item-label>
+            <q-item-label caption class="text-message">
+              Lorem ipsum, dolor sit amet consectetur adipisicing
+              elit.</q-item-label
+            >
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      :width="180"
+      show-if-above
+      side="right"
+      bordered
+    >
+      <!-- drawer content -->
+    </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref<boolean>(false);
+</script>
+
+<style scoped lang="scss">
+.q-drawer {
+  height: 100vh;
+}
+
+.q-item__label {
+  font-size: 20px;
+  color: #4399ff;
+}
+
+.q-input {
+  width: 357px;
+  height: 30px;
+  background: #fff;
+}
+
+.text-message {
+  color: #959595;
+  font-size: 12px;
+}
+</style>
--- frontend/src/pages/service.vue
+++ frontend/src/pages/service.vue
@@ -1,5 +1,50 @@
 <template>
-  <div>teste</div>
+  <div>
+    <div class="row q-pt-md q-pl-xl items-center">
+      <q-img :src="image" />
+      <p class="q-ma-none q-pl-md">Nome do Cliente</p>
+    </div>
+    <q-separator class="q-mt-md" />
+
+    <div class="row messages">teste</div>
+    <div class="row send-message items-center justify-center">
+      <q-input v-model="textSpanContainsTextSpan" outlined />
+      <q-btn class="q-ml-sm"> Enviar </q-btn>
+    </div>
+  </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped lang="scss"></style>
+<script setup lang="ts">
+definePageMeta({
+  layout: 'message'
+});
+
+const image = new URL(`../core/assets/images/bot-image.png`, import.meta.url)
+  .href;
+</script>
+
+<style scoped lang="scss">
+.q-img {
+  height: 70px;
+  width: 70px;
+}
+
+p {
+  color: #4399ff;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+}
+
+.messages {
+  height: 80vh;
+}
+
+.send-message {
+  height: 8.8vh;
+}
+
+.q-input {
+  width: 80%;
+}
+</style>
